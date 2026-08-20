40681370eed784210b22cb989d46cc78d2378985
siyabonga mazibuko <siyabongasamkelociam@gmail.com>
2026-08-20 22:58:35 +0200

    feat: complete syndicate group routing module and map all application endpoints into server engine

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes";
+import syndicateRoutes from "./routes/syndicateRoutes";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/syndicates", syndicateRoutes);
 
 app.get("/test", (req: Request, res: Response) => {
   res.status(200).json({
