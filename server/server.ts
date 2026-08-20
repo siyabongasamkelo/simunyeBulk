a6a0c4e678ae17371b1d4810c484f7c7a003c0e6
siyabonga mazibuko <siyabongasamkelociam@gmail.com>
2026-08-20 16:43:33 +0200

    feat: configure complete user routes module with zod validators and clean semantic layout

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1/users", userRoutes);
 
 app.get("/test", (req: Request, res: Response) => {
   res.status(200).json({
